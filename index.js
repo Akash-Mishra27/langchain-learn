853bfc572254bbb06f3266d8b29d74078a8750d4
Akash-VR46 <akash.36mishra94@gmail.com>
2025-11-26 20:10:15 +0530

    Refactor chat prompt template for clarity and consistency

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ import "dotenv/config";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { ChatPromptTemplate, HumanMessagePromptTemplate } from "@langchain/core/prompts";
 const model = new ChatGoogleGenerativeAI({
-        temperature: 0.7,
-        model: "gemini-2.5-flash",
-        apiKey: process.env.GOOGLE_API_KEY,
-    });
+    temperature: 0.7,
+    model: "gemini-2.5-flash",
+    apiKey: process.env.GOOGLE_API_KEY,
+});
 async function chat() {
-    const template = ChatPromptTemplate.fromMessages([
-        ["system","explain topic with in 100 words"],
-        ["human","Explain about {topic} in brief."]
-    ]);
-    const formattedMessage = await template.formatMessages({ topic: "javascript" });
-    
+    const template = ChatPromptTemplate.fromTemplate("explain {topic} with in {words} words");
+    const formattedMessage = await template.formatMessages({
+        topic: "javascript"
+        , words: 50
+    });
+
     const response = await model.invoke(formattedMessage);
     console.log("Response:", response.content);
 }
